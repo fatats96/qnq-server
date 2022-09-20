c5aedd78697bc9f364d8c46414dd2ebc5b06ed48
fatats96 <cefatihates@gmail.com>
2022-09-20 16:50:56 +0300

    updated movie controller

--- QandQ.MovieAPI/Controllers/MovieController.cs
+++ QandQ.MovieAPI/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using QandQ.Core;
 using QandQ.Core.Models;
 using QandQ.Core.Repositories;
